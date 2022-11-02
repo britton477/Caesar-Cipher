f19b686ccc3dfc295eaa9b050db1475609d4461e
JamsBond <34511634+britton477@users.noreply.github.com>
2022-11-02 19:22:04 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+print(art.logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+if shift >= 26:
+    shift = shift % 26
+
+
+def cipher(message=text, num=shift, choice=direction):
+    new_message = []
+
+    for letter in message:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if choice == "encode":
+                new_position = position + num
+            else:
+                new_position = position - num
+            new_message.append(alphabet[new_position])
+
+    new_message = ''.join(new_message)
+    print(f"Your {direction}d message is {new_message}")
+
+
+cipher()
